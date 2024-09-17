401c55db20836afaa650c074c9483db79964b826
German Uriarte <uriarte.german@gmail.com>
2024-09-17 15:38:42 -0400

    basic working calculator

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let operatorClicked;
 function operate(a, b, operator) {
   a = parseFloat(a);
   b = parseFloat(b);
+  console.log(`Inside operate: ${a} ${b} ${operator}`);
+
   switch (operator) {
     case "+":
       console.log("here");
@@ -35,12 +37,15 @@ const display = document.querySelector(".display");
 const number = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const clearButton = document.querySelector(".clear-button");
+const equalsButton = document.querySelector(".equals-button");
 
 number.forEach((element) => {
   element.addEventListener("click", () => {
     if (operatorClicked) {
       display.textContent = "";
       operatorClicked = false;
+      display.textContent += `${element.textContent}`;
+      return;
     }
     display.textContent += `${element.textContent}`;
   });
@@ -52,17 +57,37 @@ operators.forEach((operator) => {
     if (!firstNumber) {
       firstNumber = display.textContent;
       currentOperator = operator.textContent;
+      console.log(
+        `Inside first if: ${firstNumber}, ${secondNumber}, ${currentOperator}`
+      );
       return;
     }
-    console.log(firstNumber, currentOperator);
+    console.log(
+      `Before operate: ${firstNumber}, ${secondNumber}, ${currentOperator}`
+    );
 
     secondNumber = display.textContent;
-    display.textContent = "";
+    display.textContent = firstNumber;
     firstNumber = operate(firstNumber, secondNumber, currentOperator);
+    currentOperator = operator.textContent;
+    console.log(
+      `After operate: ${firstNumber}, ${secondNumber}, ${currentOperator}`
+    );
     display.textContent = firstNumber;
   });
 });
 
 clearButton.addEventListener("click", () => {
-  display.textContent = "";
+  display.textContent = "0";
 });
+
+equalsButton.addEventListener("click", () => {
+  secondNumber = display.textContent;
+  display.textContent = operate(firstNumber, secondNumber, currentOperator);
+});
+
+function resetCalculator() {
+  firstNumber = "";
+  secondNumber = "";
+  currentOperator = "";
+}
